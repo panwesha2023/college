e4f01d271fc2ae5a28139e4159b872b1be83678e
panwesha2023 <panwesha2023@gmail.com>
2025-08-12 11:35:05 +0530

    Update input2.py

--- python/input2.py
+++ python/input2.py
@@ -1,1 +1,8 @@
 #input 2 numbers and print the summation
+a = int(input("Enter the value of a "))
+b = int(input("Enter the value of b "))
+sum = 0
+sum = a + b
+print("a= ",a)
+print("b= ",b)
+print("summation= ",sum)
