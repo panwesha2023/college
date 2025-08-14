862852b9d07cd33357277f20775f38d2e0c1b86d
panwesha2023 <panwesha2023@gmail.com>
2025-08-14 11:34:13 +0530

    Create input7.py

---
+++ python/input7.py
@@ -0,0 +1,7 @@
+#calculate the simple interest
+p = float(input("Enter the value of p "))
+t = float(input("Enter the value of t "))
+r = float(input("Enter the value of r "))
+si = 0
+si = (p * t * r)/100
+print("simple interest=",si)
