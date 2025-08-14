c919d0d5bc79d56d15b8cb2802e9993cf49168b7
panwesha2023 <panwesha2023@gmail.com>
2025-08-14 11:40:43 +0530

    Create ifelse1.py

---
+++ python/ifelse1.py
@@ -0,0 +1,7 @@
+#input 2 numbers and print the greatest
+a = int(input("Enter value of a "))
+b = int(input("Enter value of b "))
+if(a > b):
+  print(a,"is greater")
+else:
+  print(b,"is greater")
