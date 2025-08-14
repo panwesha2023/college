612d80626d2ed83c7c1f460925492d636eb51568
panwesha2023 <panwesha2023@gmail.com>
2025-08-14 11:23:31 +0530

    Create input4.py

---
+++ python/input4.py
@@ -0,0 +1,7 @@
+#input 4 numbers and print the average
+a = int(input("Enter the value of a "))
+b = int(input("Enter the value of b "))
+c = int(input("Enter the value of c "))
+d = int(input("Enter the value of d "))
+avg = (a+b+c+d)/4
+print("average=",avg)
