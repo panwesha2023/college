575997901217ade9d3b09f7e4d33dd136edc844c
panwesha2023 <panwesha2023@gmail.com>
2025-08-14 11:26:15 +0530

    Create input5.py

---
+++ python/input5.py
@@ -0,0 +1,6 @@
+#input width and length and print the area of rectangle
+w = int(input("Enter the value of w "))
+l = int(input("Enter the value of l "))
+area = 0
+area = w * l
+print("area=",area)
