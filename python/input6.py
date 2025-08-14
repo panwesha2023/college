51be1a49d81b2f85313d9614b6be8c3a2ee5baa5
panwesha2023 <panwesha2023@gmail.com>
2025-08-14 11:31:36 +0530

    Create input6.py

---
+++ python/input6.py
@@ -0,0 +1,9 @@
+#print the area and perimeter of a circle
+pie = float(input("Enter the value of pie "))
+r = float(input("Enter the value of r "))
+area = 0
+peri = 0
+area = pie * r * r
+peri = 2 * pie * r
+print("area = ",area)
+print("perimeter = ",peri)
