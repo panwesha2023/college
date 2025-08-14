b514459aeec83f553de276e47a7599c48d317462
panwesha2023 <panwesha2023@gmail.com>
2025-08-14 11:19:56 +0530

    Create input3.py

---
+++ python/input3.py
@@ -0,0 +1,6 @@
+#input 3 numbers and print the multiplication
+a = int(input("Enter the value of a "))
+b = int(input("Enter the value of b "))
+c = int(input("Enter the value of c "))
+multi = a * b * c
+print("multiplication = ",multi)
